1fe21a3a81ecbcf8223415969dc96843123979a7
Calvin-Davidson <calvin2003davidson@gmail.com>
2021-05-24 18:30:24 +0200

    Engine.js and Loader.js
    The engine.js will manage all required script for the Physics engine to work.
    
    Loader.js will be used by Engine.js to load script dynamicly. So that you only need 1 script imported in your html.

---
+++ core/Engine.js
@@ -0,0 +1,4 @@
+// Loading the Loader.js script. so i can load all other required scripts.
+const script = document.createElement('script');
+script.src = "../PhysicsEngine/core/Loader.js";
+document.head.appendChild(script);
