369e9e8fb84d3946b7d844f7c8e77d2a8f284420
Calvin-Davidson <calvin2003davidson@gmail.com>
2021-08-02 11:10:53 +0200

    Added a ConfineInScreen class, that can confine the cube and circle object to be stuck within the screen.

---
+++ Colliders/ConfineInScreen.ts
@@ -0,0 +1,28 @@
+class ConfineInScreen {
+    static ConfineCubeInScreen(cube: Cube) {
+        if (cube.position.x - cube.width / 2 <= 0)
+            cube.velocity.x = Math.abs(cube.velocity.x)
+        if (cube.position.x + cube.width / 2 >= window.innerWidth)
+            cube.velocity.x = -Math.abs(cube.velocity.x);
+
+        if (cube.position.y - cube.height / 2 <= 0)
+            cube.velocity.y = Math.abs(cube.velocity.y);
+        if (cube.position.y + cube.height / 2 >= window.innerHeight)
+            cube.velocity.y = -Math.abs(cube.velocity.y);
+    }
+
+    static ConfineCircleInScreen(circle: Circle) {
+        if (circle.position.x - circle.radius <= 0) {
+            circle.velocity.x = Math.abs(circle.velocity.x);
+        }
+        if (circle.position.x + circle.radius >= window.innerWidth) {
+            circle.velocity.x = -Math.abs(circle.velocity.x);
+        }
+        if (circle.position.y - circle.radius <= 0) {
+            circle.velocity.y = Math.abs(circle.velocity.y);
+        }
+        if (circle.position.y + circle.radius >= window.innerHeight) {
+            circle.velocity.y = -Math.abs(circle.velocity.y);
+        }
+    }
+}
